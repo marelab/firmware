0a91fa3e048daa24bbcf9c477322f9ae52b61167
marc hammermann <marchammermann@googlemail.com>
2024-02-28 22:17:14 +0100

    DOCKER

--- docker/startmesh.sh
+++ docker/startmesh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 RUNDIR=$(pwd)
-echo $RUNDIR
+#echo $RUNDIR
 source export HOME=$RUNDIR
 sh -cx "~/meshtasticd_linux_$(arch) --hwid '${HWID:-$RANDOM}'"
 #docker run -it -p 8080:9001 -v ~/meshmnt:/home/mesh/dockerhost meshtastic bash
