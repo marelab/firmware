9d7a213f21eba90788133dcabc7e4f7e37378188
marc hammermann <marchammermann@googlemail.com>
2024-02-29 12:47:41 +0100

    docker

--- docker/startmesh.sh
+++ docker/startmesh.sh
@@ -19,10 +19,10 @@ while true; do
 
 	case $option in
 	1)
-		sh -cx "$HOME/meshtasticd_linux_$(arch) --hwid '${HWID:-$RANDOM}'--config '$HOME/config.yaml'"
+		sh -cx "$HOME/meshtasticd_linux_$(arch) --hwid '${HWID:-$RANDOM}'--config '$HOME/config-dist.yaml'"
 		;;
 	2)
-		sh -cx "$HOME/meshtasticd_linux_$(arch) --hwid '${HWID:-$RANDOM}' --config '$HOME/config.yaml'"
+		sh -cx "$HOME/meshtasticd_linux_$(arch) --hwid '${HWID:-$RANDOM}' -c '$HOME/config.yaml'"
 		;;
 	3)
 		echo "Beende das Skript..."
